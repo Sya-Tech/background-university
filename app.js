298212807a056085ffb6f0a8c9cd9938cc11b57a
George Chung <Gerhut@GMail.com>
2014-08-17 18:20:51 +0800

    change a lot

--- app.js
+++ app.js
@@ -1,77 +1,93 @@
-void function () {
-  var data = {}
-  var status = {}
+void function ($) {
   var chart = c3.generate({
     bindto: '#chart',
-    size: { height: 500 },
+    size: { height: 480 },
     data: {
-      type: 'pie',
-      json: {},
-      onclick: function (d) {
-        if (status.university === true || typeof status.background === 'string')
-          return university(d.id)
-        if (status.background === true || typeof status.university === 'string')
-          return background(d.id)
-      }
+      type: 'bar',
+      x : 'x',
+      rows: []
+    },
+    axis: {
+      x: { type: 'category' }
     }
   })
 
-  var apply = function (title, json) {
-    document.getElementById('title').innerText = title
-    chart.load({
-      json: json,
-      unload: true
-    })
-  }
+  var demoTimeout = -1
 
-  var university = function (name) {
-    var json = {}
-    var title = name ? name + '大学与背景的关系' : '大学占比'
-    if (name) {
-      json = data[name]
-    } else {
-      for (var u in data) {
-        json[u] = 0
-        for (var b in data[u]) {
-          json[u] += data[u][b]
-        }
-      }
-    }
-    status = { university: name || true }
-    apply(title, json)
-  }
-
-  var background = function (name) {
-    var json = {}
-    var title = name ? name + '背景与大学的关系' : '背景占比'
-    if (name) {
-      for (var u in data) {
-        json[u] = data[u][name]
-      }
-    } else {
-      for (var u in data) {
-        for (var b in data[u]) {
-          json[b] = (json[b] || 0) + data[u][b]
-        }
-      }
-    }
-    status = { background: name || true }
-    apply(title, json)
-  }
+  var $title = $('#title')
+  var $chartList = $('.chart-list')
 
   d3.json('data.json', function (err, json) {
-    if (err) return alert(err.statusText)
-    data = json
+    if (err)
+      return $title.text(err.statusText)
+    $title.text('数据处理中……')
 
-    document.getElementById('background')
-      .addEventListener('click', function () {
-        background()
-      }, false)
-    document.getElementById('university')
-      .addEventListener('click', function () {
-        university()
-      }, false)
+    var majors = {}
 
-    background()
+    function construct$chart(text, chart) {
+      return $('<a>', {
+        href: '#',
+        text: text,
+        data: { chart: chart }
+      }).addClass('list-group-item')
+    }
+
+    var $charts = $()
+    for ( var u in json ) {
+      var rows = [['x', 'paper', 'intern', 'project']]
+
+      for (var m in json[u]) {
+        rows.push([ m,
+          json[u][m].paper || 0,
+          json[u][m].intern || 0,
+          json[u][m].project || 0
+        ])
+
+        if (majors[m] == null) {
+          majors[m] = [['x', 'paper', 'intern', 'project']]
+        }
+
+        majors[m].push([ u,
+          json[u][m].paper || 0,
+          json[u][m].intern || 0,
+          json[u][m].project || 0
+        ])
+      }
+
+      $charts = $charts.add(construct$chart(u, { rows: rows }))
+    }
+    $('#university-chart-list').append($charts)
+
+    $charts = $()
+    for (var m in majors) {
+      $charts = $charts.add(construct$chart(m, { rows: majors[m] }))
+    }
+    $('#major-chart-list').append($charts)
+
+    $title.text('')
+
+    demoInterval = setInterval(function () {
+      var $active = $chartList.find('.active')
+      var $next = $active.next()
+      if ($next.length === 0) {
+        $next = $active.parent().next().children().first()
+      }
+      if ($next.length === 0) {
+        $next = $chartList.children().first()
+      }
+      $next.trigger('click', true)
+    }, 2000)
+    $chartList.children().first().trigger('click', true)
   })
-} ()
+
+  $chartList.on('click', '.list-group-item', function (event, automatic) {
+    var $this = $(this)
+    if (!automatic) {
+      clearInterval(demoInterval)
+    }
+    $title.text($this.text())
+    chart.load($this.data('chart'))
+    $chartList.find('.list-group-item').removeClass('active')
+    $this.addClass('active')
+  })
+} (jQuery)
