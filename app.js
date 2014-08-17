4c3a174eb1024c208a9775792671db9754e59316
George Chung <Gerhut@GMail.com>
2014-08-17 18:57:30 +0800

    add pie

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ void function ($) {
     bindto: '#chart',
     size: { height: 480 },
     data: {
-      type: 'bar',
       x : 'x',
       rows: []
     },
@@ -23,6 +22,18 @@ void function ($) {
     $title.text('数据处理中……')
 
     var majors = {}
+    var backgrounds = {
+      universities: {
+        paper: [],
+        intern: [],
+        project: []
+      },
+      majors: {
+        paper: [],
+        intern: [],
+        project: []
+      }
+    }
 
     function construct$chart(text, chart) {
       return $('<a>', {
@@ -52,17 +63,34 @@ void function ($) {
           json[u][m].intern || 0,
           json[u][m].project || 0
         ])
+
+        backgrounds.universities.paper.push([u, json[u][m].paper])
+        backgrounds.majors.paper.push([m, json[u][m].paper])
+        backgrounds.universities.intern.push([u, json[u][m].intern])
+        backgrounds.majors.intern.push([m, json[u][m].intern])
+        backgrounds.universities.project.push([u, json[u][m].project])
+        backgrounds.majors.project.push([m, json[u][m].project])
       }
 
-      $charts = $charts.add(construct$chart(u, { rows: rows }))
+      $charts = $charts.add(construct$chart(u, { rows: rows, type: 'bar' }))
     }
     $('#university-chart-list').append($charts)
 
     $charts = $()
     for (var m in majors) {
-      $charts = $charts.add(construct$chart(m, { rows: majors[m] }))
+      $charts = $charts.add(construct$chart(m, { rows: majors[m], type: 'bar' }))
     }
     $('#major-chart-list').append($charts)
+
+    $charts = $()
+    for (var b in backgrounds.universities) {
+      $('#' + b + '-chart-list').append(
+        construct$chart(b + ' 按院校', { columns: backgrounds.universities[b], type: 'pie' }))
+    }
+    for (var b in backgrounds.majors) {
+      $('#' + b + '-chart-list').append(
+        construct$chart(b + ' 按专业', { columns: backgrounds.majors[b], type: 'pie' }))
+    }
 
     $title.text('')
 
@@ -80,13 +108,24 @@ void function ($) {
     $chartList.children().first().trigger('click', true)
   })
 
+  var currentType = null
+
   $chartList.on('click', '.list-group-item', function (event, automatic) {
     var $this = $(this)
     if (!automatic) {
       clearInterval(demoInterval)
+      demoInterval = -1
     }
     $title.text($this.text())
-    chart.load($this.data('chart'))
+    
+    var data = $this.data('chart')
+    if (currentType !== data.type) {
+      currentType = data.type
+      chart.load($.extend(data, { unload: true }))
+    } else {
+      chart.load(data)
+    }
+    
     $chartList.find('.list-group-item').removeClass('active')
     $this.addClass('active')
   })
