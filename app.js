49784f6fcd205e9f4d5f5cc9fe66e7e8dfcec660
George Chung <Gerhut@GMail.com>
2014-08-17 14:20:39 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+void function () {
+  var data = {}
+  var status = {}
+  var chart = c3.generate({
+    bindto: '#chart',
+    size: { height: 500 },
+    data: {
+      type: 'pie',
+      json: {},
+      onclick: function (d) {
+        if (status.university === true || typeof status.background === 'string')
+          return university(d.id)
+        if (status.background === true || typeof status.university === 'string')
+          return background(d.id)
+      }
+    }
+  })
+
+  var apply = function (title, json) {
+    chart.load({
+      json: json,
+      unload: true
+    })
+    console.dir(chart)
+  }
+
+  var university = function (name) {
+    var json = {}
+    var title = (name || '') + '大学与背景的关系'
+    if (name) {
+      json = data[name]
+    } else {
+      for (var u in data) {
+        json[u] = 0
+        for (var b in data[u]) {
+          json[u] += data[u][b]
+        }
+      }
+    }
+    status = { university: name || true }
+    apply(title, json)
+  }
+
+  var background = function (name) {
+    var json = {}
+    var title = (name || '') + '背景与大学的关系'
+    if (name) {
+      for (var u in data) {
+        json[u] = data[u][name]
+      }
+    } else {
+      for (var u in data) {
+        for (var b in data[u]) {
+          json[b] = (json[b] || 0) + data[u][b]
+        }
+      }
+    }
+    status = { background: name || true }
+    apply(title, json)
+  }
+
+  d3.json('data.json', function (err, json) {
+    if (err) return alert(err.statusText)
+    data = json
+
+    document.getElementById('background')
+      .addEventListener('click', function () {
+        background()
+      }, false)
+    document.getElementById('university')
+      .addEventListener('click', function () {
+        university()
+      }, false)
+
+    background()
+  })
+} ()
